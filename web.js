47afd3cf13e9de67c709722eb94445cc3d0fc133
Ubuntu <ubuntu@ip-172-31-6-218.us-west-2.compute.internal>
2013-07-11 03:29:00 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
-var express = require('express');
+fs=require('fs');
+var inputFile='~/bitstarter/index.html';
 
-var app = express.createServer(express.logger());
+fs.readFileSync(inputFile, 'utf-8', function(err,data){
+ var strResult=data;
+ var express = require('express');
+ var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+ app.get('/', function(request, response) {
+  response.send(strResult);
+ });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
   console.log("Listening on " + port);
+ });
 });
