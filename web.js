f97899a1d36740c8718884f909102a21cf0d37ca
Ubuntu <ubuntu@ip-172-31-6-218.us-west-2.compute.internal>
2013-07-14 06:01:21 +0000

    updated file

--- web.js
+++ web.js
@@ -2,19 +2,18 @@
 var fs=require('fs');
 var inputFile="index.html";
 
-fs.readFileSync(inputFile, 'utf8', function(err,data){
- var strResult=data;
- var express = require('express');
- var app = express.createServer(express.logger());
+var strOut=fs.readFileSync(inputFile, 'utf8');
+
+var express = require('express');
+var app = express.createServer(express.logger());
 
  app.get('/', function(request, response) {
-  response.send(strResult);
+  response.send(strOut);
  });
 
-if(err) throw err;
 
- var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
  app.listen(port, function() {
   console.log("Listening on " + port);
  });
-});
+
